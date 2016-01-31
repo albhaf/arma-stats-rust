987b8beedd00ab99505a29eb5706a08ef1b59025
Albert Hafvenström <albhaf@gmail.com>
2016-01-31 21:36:26 +0100

    fix windows calling convention and disable linux build

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-cargo build --target=x86_64-unknown-linux-gnu --release
+#TODO: enable linux build when it's supported
+#cargo build --target=x86_64-unknown-linux-gnu --release
 cargo build --target=x86_64-pc-windows-gnu --release
--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,12 @@ use libc::c_char;
 use libc::c_int;
 use libc::strncpy;
 
+#[cfg(target_os = "windows")]
 #[allow(non_snake_case)]
 #[no_mangle]
-pub extern "system" fn _RVExtension(output: *mut c_char,
-                                    output_size: c_int,
-                                    function: *const c_char) {
+pub extern "stdcall" fn _RVExtension(output: *mut c_char,
+                                     output_size: c_int,
+                                     function: *const c_char) {
     let size = output_size as usize - 1;
     unsafe {
         strncpy(output, function, size);
