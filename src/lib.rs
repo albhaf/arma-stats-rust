205bd6101fde8ed7d08bb889bd3e771aa5a4aaba
Albert Hafvenström <albhaf@gmail.com>
2016-02-14 19:24:33 +0100

    add explicit export_name for RVExtension

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,10 @@ use libc::strncpy;
 #[cfg(target_os = "windows")]
 #[allow(non_snake_case)]
 #[no_mangle]
-pub extern "stdcall" fn _RVExtension(output: *mut c_char,
-                                     output_size: c_int,
-                                     function: *const c_char) {
+#[export_name="_RVExtension@12"]
+pub extern "stdcall" fn RVExtension(output: *mut c_char,
+                                    output_size: c_int,
+                                    function: *const c_char) {
     let size = output_size as usize - 1;
     unsafe {
         strncpy(output, function, size);
