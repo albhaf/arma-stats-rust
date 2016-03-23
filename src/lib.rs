30fde650d5f44aa92b8dd6677344bb56ed09d0b5
Albert Hafvenström <albhaf@gmail.com>
2016-03-23 15:31:20 +0000

    remove explicit windows cfg target, add parsing of function, add simple echo test

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,48 @@ extern crate libc;
 use libc::c_char;
 use libc::c_int;
 use libc::strncpy;
+use std::ffi::CStr;
+use std::str;
 
-#[cfg(target_os = "windows")]
 #[allow(non_snake_case)]
 #[no_mangle]
 #[export_name="_RVExtension"]
-pub extern "stdcall" fn RVExtension(output: *mut c_char,
-                                    output_size: c_int,
-                                    function: *const c_char) {
-    let size = output_size as usize - 1;
+pub extern "system" fn RVExtension(output: *mut c_char,
+                                   _output_size: c_int,
+                                   function: *const c_char) {
+    let c_str = unsafe {
+        assert!(!function.is_null());
+        CStr::from_ptr(function)
+    };
+
+    let r_str = str::from_utf8(c_str.to_bytes()).unwrap();
+
+    let input: Vec<&str> = r_str.split(";").collect();
+
+    let function_name = input[0];
+
     unsafe {
-        strncpy(output, function, size);
+        strncpy(output,
+                function_name.as_ptr() as *const c_char,
+                function_name.len() as usize);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::ffi::CString;
+
+    #[test]
+    fn function_echo() {
+        let function = CString::new("foo;bar").unwrap();
+        let out = CString::new("").unwrap().into_raw();
+        RVExtension(out,
+                    4096, // game currently calls method with this value
+                    function.as_ptr());
+
+        let result = unsafe { CString::from_raw(out) };
+        assert_eq!("foo", result.into_string().unwrap())
     }
 }
