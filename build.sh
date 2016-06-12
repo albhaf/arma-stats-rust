66e4dc1780d25a2bc4f9081764a5477ac8644c9b
Albert Hafvenström <albhaf@gmail.com>
2016-06-12 18:36:20 +0200

    move openssl variables to docker

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #TODO: enable linux build when it's supported
 #cargo build --target=x86_64-unknown-linux-gnu --release
 
-export OPENSSL_LIB_DIR="/usr/i686-w64-mingw32/bin"
-export OPENSSL_INCLUDE_DIR="/usr/i686-w64-mingw32/include"
-
 cargo build --target=i686-pc-windows-gnu --release --verbose
 
